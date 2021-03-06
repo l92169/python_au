64427c2c15fd999374d6ff080d0eb48ad4082d31
l92169 <71372570+l92169@users.noreply.github.com>
2021-03-06 16:48:28 +0300

    REQUESTS-1022 Refactored pulls and committs.py

--- requests/pulls and committs.py
+++ requests/pulls and committs.py
@@ -1,16 +1,17 @@
 import requests
 import json
-PREFIXES = ['GENERATOR', 'LEETCODE', 'HEXNUMBER','ITERATOR', 'TRIANGLE']
-GROUPS = ['1011','1012','1021','1022']
-ACTION = ['Added','Deleted','Refactored','Fixed','Moved']
-TOKEN = '1d68ea6cbb80c9032005710d5c3e34d228b3895d'
+
+PREFIXES = ['GENERATOR', 'LEETCODE', 'HEXNUMBER', 'ITERATOR', 'TRIANGLE']
+GROUPS = ['1011', '1012', '1021', '1022']
+ACTION = ['Added', 'Deleted', 'Refactored', 'Fixed', 'Moved']
+TOKEN = ''
 
 
 def prepare_headers():
-    return{'Authorization': 'token ' + TOKEN,
-           'Content-Type': "application/json",
-           'Accept': "application/vnd.github.v3+json"
-    }
+    return {'Authorization': 'token ' + TOKEN,
+            'Content-Type': "application/json",
+            'Accept': "application/vnd.github.v3+json"
+            }
 
 
 def check_prefixes(title):
@@ -25,38 +26,46 @@ def check_prefixes(title):
     if group not in GROUPS:
         TF = False
         result.append('Pull Request title must contain group number in {}'.format(GROUPS))
-    if title[1] not in ACTION or len(title)<2:
+    if title[1] not in ACTION or len(title) < 2:
         TF = False
         result.append('Pull Request action must contain action in {}'.format(ACTION))
     return '/n'.join(result), TF
 
 
 def send_pr_comment(pr, errors):
-    r = requests.post(pr['url']+'/comments', headers = prepare_headers(), data = {'body': '{}'.format(errors),
-                                                                                  'path': requests.get(pr['url']+'/files', headers = prepare_headers()).json()[0]['filename'],
-                                                                                  'position': 1,
-                                                                                  'commit_id': pr['head']['sha']})
+    r = requests.post(pr['url'] + '/comments', headers=prepare_headers(), json={'body': '{}'.format(errors),
+                                                                                'path':
+                                                                                    requests.get(pr['url'] + '/files',
+                                                                                                 headers=prepare_headers()).json()[
+                                                                                        0]['filename'],
+                                                                                'position': 1,
+                                                                                'commit_id': pr['head']['sha']})
     print(r.status_code)
+    print(r.json())
 
 
 def get_all_user_pr(user_login, repos_name, pr_state):
     url = 'https://api.github.com/repos/{}/{}/pulls?state={}'.format(str(user_login), str(repos_name), str(pr_state))
-    pulls = requests.get(url, headers = prepare_headers())
+
+    pulls = requests.get(url, headers=prepare_headers())
     return pulls
 
 
 def get_all_pr_commits(pr):
     url = pr['commits_url']
-    commits = requests.get(url, headers = prepare_headers())
+    commits = requests.get(url, headers=prepare_headers())
     return commits
 
 
 def main():
     user = 'l92169'
-    repos ='python_au'
+    repos = 'python_au'
     state = 'open'
     for pr in (get_all_user_pr(user, repos, state)).json():
         if not check_prefixes(pr['title'])[1]:
-          send_pr_comment(pr, check_prefixes(pr['title'])[0])
+            print(pr['title'])
+            send_pr_comment(pr, check_prefixes(pr['title'])[0])
+
+
 main()
 
