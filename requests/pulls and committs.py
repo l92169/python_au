fd3b7842bc894258c605192653ce83797efd7310
l92169 <tempsp@mail.ru>
2021-03-06 22:12:00 +0300

    REQUESTS-1022 Refactored pulls and commits.py

--- requests/pulls and committs.py
+++ requests/pulls and committs.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-PREFIXES = ['GENERATOR', 'LEETCODE', 'HEXNUMBER', 'ITERATOR', 'TRIANGLE']
+TASK_PREFIX = ['LEETCODE', 'GENERATOR', 'TRIANGLE', 'HEXNUMBER', 'REQUESTS', 'ITERATOR']
 GROUPS = ['1011', '1012', '1021', '1022']
 ACTION = ['Added', 'Deleted', 'Refactored', 'Fixed', 'Moved']
-TOKEN = ''
+TOKEN = '7a32f69e1f833088fca51bc0b00ff2c88708d8ca'
 
 
 def prepare_headers():
@@ -20,33 +20,31 @@ def check_prefixes(title):
     title = title.split()
     prefixes = title[0].split('-')
     pull_prefix, group = prefixes[0], prefixes[1]
-    if pull_prefix not in PREFIXES:
+    if pull_prefix not in TASK_PREFIX:
         TF = False
-        result.append('Pull Request prefix must contain prefix in {}'.format(PREFIXES))
+        result.append('The prefix of your message must contain prefix in {}'.format(TASK_PREFIX))
     if group not in GROUPS:
         TF = False
-        result.append('Pull Request title must contain group number in {}'.format(GROUPS))
+        result.append('The group number of your message must contain group number in {}'.format(GROUPS))
     if title[1] not in ACTION or len(title) < 2:
         TF = False
-        result.append('Pull Request action must contain action in {}'.format(ACTION))
+        result.append('The action of your message must contain action in {}'.format(ACTION))
     return '/n'.join(result), TF
 
 
+def prepare_body(pr, errors):
+    return {'body': '{}'.format(errors),
+            'path': requests.get(pr['url'] + '/files', headers=prepare_headers()).json()[0]['filename'],
+            'position': 1,
+            'commit_id': pr['head']['sha']}
+
+
 def send_pr_comment(pr, errors):
-    r = requests.post(pr['url'] + '/comments', headers=prepare_headers(), json={'body': '{}'.format(errors),
-                                                                                'path':
-                                                                                    requests.get(pr['url'] + '/files',
-                                                                                                 headers=prepare_headers()).json()[
-                                                                                        0]['filename'],
-                                                                                'position': 1,
-                                                                                'commit_id': pr['head']['sha']})
-    print(r.status_code)
-    print(r.json())
+    r = requests.post(pr['url'] + '/comments', headers=prepare_headers(), json=prepare_body(pr, errors))
 
 
 def get_all_user_pr(user_login, repos_name, pr_state):
-    url = 'https://api.github.com/repos/{}/{}/pulls?state={}'.format(str(user_login), str(repos_name), str(pr_state))
-
+    url = 'https://api.github.com/repos/{}/{}/pulls?state={}'.format(user_login, repos_name, pr_state)
     pulls = requests.get(url, headers=prepare_headers())
     return pulls
 
@@ -57,15 +55,24 @@ def get_all_pr_commits(pr):
     return commits
 
 
+def verify_pr(pr):
+    message = 'Your pull request {}/n'.format(pr['title'])
+    message += check_prefixes(pr['title'])[0]
+    message += '\n\n'
+    for commit in get_all_pr_commits(pr).json():
+        comm = commit['commit']
+        message += 'Your commit: {}\n'.format(comm['message'])
+        message += check_prefixes(comm['message'])[0]
+        message += '\n'
+    return send_pr_comment(pr, create_message(pr))
+
+
 def main():
-    user = 'l92169'
+    user = 'alexarlord-boop'
     repos = 'python_au'
     state = 'open'
     for pr in (get_all_user_pr(user, repos, state)).json():
-        if not check_prefixes(pr['title'])[1]:
-            print(pr['title'])
-            send_pr_comment(pr, check_prefixes(pr['title'])[0])
-
-
+        errors, TF = check_prefixes(pr['title'])[0], check_prefixes(pr['title'])[1]
+        if not TF:
+            verify_pr(pr)
 main()
-
