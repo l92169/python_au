56f6f194808952cdddb757ec6e9436418c343533
Elizaveta Ilina <71372570+l92169@users.noreply.github.com>
2021-03-20 11:54:05 +0300

    REQUESTS-1022 Refactored pulls and commits
    Check last comment

--- requests/pulls and committs.py
+++ requests/pulls and committs.py
@@ -1,18 +1,16 @@
 import requests
 import json
-
-
-TASK_PREFIX = ['GENERATOR', 'LEETCODE', 'HEXNUMBER','ITERATOR', 'TRIANGLE']
-GROUPS = ['1011','1012','1021','1022']
-ACTION = ['Added','Deleted','Refactored','Fixed','Moved']
-TOKEN = ''
+TASK_PREFIX = ['GENERATOR', 'LEETCODE', 'HEXNUMBER', 'ITERATOR', 'TRIANGLE', 'REQUESTS']
+GROUPS = ['1011','1012','1021','1022'] 
+ACTION = ['Added', 'Deleted', 'Refactored', 'Fixed', 'Moved']
+TOKEN = '655568f0dd9176541d48450ed1de03acf4774d40'
 
 
 def prepare_headers():
     return{'Authorization': 'token ' + TOKEN,
            'Content-Type': "application/json",
            'Accept': "application/vnd.github.v3+json"
-    }
+           }
 
 
 def check_prefixes(title):
@@ -49,7 +47,7 @@ def send_pr_comment(pr, errors):
 
 
 def get_all_user_pr(user_login, repos_name, pr_state):
-    url = 'https://api.github.com/repos/{}/{}/pulls?state={}'.format(user_login, repos_name, pr_state)
+    url = 'https://api.github.com/repos/{}/{}/pulls?state={}'.format(str(user_login), str(repos_name), str(pr_state))
     pulls = requests.get(url, headers = prepare_headers())
     return pulls
 
@@ -60,18 +58,43 @@ def get_all_pr_commits(pr):
     return commits
 
 
+def get_time_last_comment(pr):
+    url = pr['review_comments_url']
+    comments = requests.get(url, headers=prepare_headers())
+    time_last_comment = "0000-00-00T00:00:00Z"
+    for comment in comments.json():
+        time_last_comment = comment['created_at']
+    return time_last_comment
+
+
+def get_time_commit(commit):
+    date = commit['commit']['author']['date']
+    return date
+
+
+def check_time(time_last_comment, time_commit):
+    if time_last_comment > time_commit:
+        return False
+    else:
+        return True
+
+
 def verify_pr(pr):
     message = 'Your pull request {}/n'.format(pr['title'])
+    get_time_last_comment(pr)
     t, TF = check_prefixes(pr['title'])[0], check_prefixes(pr['title'])[1]
     message += t
     message += '\n\n'
+    time_last_comment = get_time_last_comment(pr)
     for commit in get_all_pr_commits(pr).json():
-        comm = commit['commit']
-        message += 'Your commit: {}\n'.format(comm['message'])
-        com, TF_com = check_prefixes(comm['message'])[0], check_prefixes(comm['message'])[1]
-        TF = TF * TF_com
-        message += com
-        message += '\n'
+        time_commit = get_time_commit(commit)
+        if check_time(time_last_comment, time_commit):
+            comm = commit['commit']
+            message += 'Your commit: {}\n'.format(comm['message'])
+            com, TF_com = check_prefixes(comm['message'])[0], check_prefixes(comm['message'])[1]
+            TF = TF * TF_com
+            message += com
+            message += '\n'
     if not TF: 
         return send_pr_comment(pr, message)
 
