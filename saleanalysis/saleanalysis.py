2b38814383914732317730a0e238e072fece5cb8
l92169 <71372570+l92169@users.noreply.github.com>
2021-05-22 14:00:26 +0300

    SALEANALYSIS-1022 Refactored saleanalysis.py

--- saleanalysis/saleanalysis.py
+++ saleanalysis/saleanalysis.py
@@ -4,35 +4,47 @@ import random
 import math
 import matplotlib.pyplot as plt
 from datetime import datetime as dt
+year = 2020
 
-N1, N2 = 3, 5
-def generate_date_resource(data):
+
+def generate_date_resource(data, N1):
     x_data, y_data = [], []
     d = filter_data(data, 'resource', '{}'.format(N1),'exact')
     for i in range(len(d)):
         x_data.append(d[i]['date'])
-        y_data.append(d[i]['count'])
+        y_data.append(int(d[i]['count']))
     return (x_data, y_data)
 
 
-def generate_date_staff_id(data):
+def generate_date_staff_id(data, N2):
     x_data, y_data = [], []
-    d = filter_data(data, 'staff_id', '{}'.format(N1),'exact')
+    d = filter_data(data, 'staff_id', '{}'.format(N2),'exact')
     for i in range(len(d)):
         x_data.append(d[i]['date'])
-        y_data.append(d[i]['count'])
+        y_data.append(int(d[i]['count']))
     return (x_data, y_data)
 
-def drawing_resource(data):
-    x, y = generate_date_resource(data)
+
+def drawing_one_resource_(data, N1):
+    x, y = generate_date_resource(data, N1)
     plt.plot(x,y, color = 'red', marker = "o")
     plt.title("Resource {}".format(N1))
     plt.xlabel("date")
     plt.ylabel("count")
     plt.show()
 
-def drawing_staff_id(data):
-    x, y = generate_date_staff_id(data)
+
+def drawing_all_resource_(data):
+    for i in range(1,11):
+        x, y = generate_date_resource(data)
+        plt.plot(x,y, color = 'red', marker = "o")
+        plt.title("Resource {}".format(i))
+        plt.xlabel("date")
+        plt.ylabel("count")
+        plt.show()
+
+def drawing_one_staff_id(data, N2):
+    x, y = generate_date_staff_id(data, N2)
     plt.plot(x,y, color = 'red', marker = "o")
     plt.title("staff_id {}".format(N2))
     plt.xlabel("date")
@@ -40,26 +52,51 @@ def drawing_staff_id(data):
     plt.show()
 
 
+def drawing_all_staff_id(data):
+    for i in range(1,10):
+        x, y = generate_date_staff_id(data, i)
+        plt.plot(x,y, color = 'red', marker = "o")
+        plt.title("staff_id {}".format(i))
+        plt.xlabel("date")
+        plt.ylabel("count")
+        plt.show()
+
+
 def generator_file_with_staff_id():
-    file = open('input.csv', "w+")
+    file = open('input1.csv', "w+")
+    data = open_file('input.csv')
     file.write('date,resource,staff_id,count \n')
-    for i in range(10):
-        file.write("{}-{},{},{},{} \n".format(random.randint(2018, 2021),\
-                                              random.randint(1, 12),\
-                                              random.randint(1, 4),\
-                                              random.randint(1, 10),\
-                                              random.randint(0, 100)))
+    for i in range(1, len(data)):
+        d = 0
+        summ = 0
+        count_staff_id = int(data[i][2])
+        while count_staff_id > summ:
+            new_count_staff_id = random.randint(0, 100)
+            summ = summ + new_count_staff_id
+            if count_staff_id < summ:
+                summ = summ - new_count_staff_id
+                new_count_staff_id = count_staff_id - summ
+                d = 1
+            if count_staff_id > summ:
+                file.write("{},{},{},{} \n".format(data[i][0],
+                                                   data[i][1],
+                                                   random.randint(1, 10),
+                                                   new_count_staff_id))
+                print("{},{},{},{} \n".format(data[i][0],
+                                              data[i][1],
+                                              random.randint(1, 10),
+                                              new_count_staff_id))
+            if d == 1:
+                break
     file.close()
 
 
 def generator_file_without_staff_id():
     file = open('input.csv', "w+")
     file.write('date,resource,count \n')
-    for i in range(10):
-        file.write("{}-{},{},{},{} \n".format(random.randint(2018, 2021),\
-                                              random.randint(1, 12),\
-                                              random.randint(0, 4),\
-                                              random.randint(0, 100)))
+    for i in range(1,11):
+        for j in range(1, 13):
+            file.write("{}-{},{},{} \n".format(year, j, i, random.randint(0, 100)))
     file.close()
 
 def open_file(input_file):
@@ -102,10 +139,12 @@ def filter_data(data, key, value, arg):
 
 
 def main():
+    generator_file_without_staff_id()
     generator_file_with_staff_id()
-    data = sort_key(list_dict('input.csv'), 'date')
-    drawing_resource(data)
-    drawing_staff_id(data)
+    data1 = sort_key(list_dict('input.csv'), 'date')
+    data2 = sort_key(list_dict('input1.csv'), 'date')
+    drawing_one_resource_(data1,3)
+    drawing_one_staff_id(data2,5)
 
 
 if __name__ == '__main__':
